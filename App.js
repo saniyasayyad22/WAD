8ae2da83411c5c3590ffe48dc086080f97e5e436
saniyasayyad22 <saniyaasayyad2020@gmail.com>
2025-05-07 17:54:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React,{useState} from 'react'
+function Counter(){
+  let[count,setCount]=useState(0);
+  function incCount(){
+    setCount(count+1);
+  }
+  return(
+     <div>
+        <h2>Count:{count}</h2>
+        <button onClick={incCount}>Increase Count</button>
+     </div>
+  )
+}
+export default Counter;
